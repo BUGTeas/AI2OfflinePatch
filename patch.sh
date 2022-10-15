b851be5c30e6e2f995a783e698a73591bb6a23bf
BUGTeas <99161186+BUGTeas@users.noreply.github.com>
2022-10-15 18:39:10 +0800

    Improve main script

--- patch.sh
+++ patch.sh
@@ -9,7 +9,6 @@ if [ "$1" = "search" ];then
 	        elif [ "$BiN" = "none" ];then
                 	echo  $BiN
 		fi
-
 	done
 elif [ "$1" = "checkn" ];then
 	echo -e "\033[41;39mCannot find program \"$2\" please install it.\n找不到应用程序 “$2”，请将其安装。\033[0m"
@@ -22,30 +21,57 @@ elif [ "$1" = "choice" ];then
 	read -p "input \"y\" to continue / 输入 “y” 以继续 :" num
 	if [ "$num" = "y" ];then
 		echo 
-		$0 $2 $3
+		$0 $2 $3 $4
 	elif [ "$num" = "Y" ];then
 		echo 
-		$0 $2 $3
+		$0 $2 $3 $4
 	else
 		echo -e "\nOperation cancelled by User.\n操作被用户取消。"
 	fi
 	exit
 elif [ "$1" = "chkdir" ];then
 	echo -e "\033[34m\nSearching for target patch file \"BuildServer.jar\" ......\n正在寻找目标修补文件“BuildServer.jar”......\033[0m"
-	if [ -d "./BuildServer" ];then
-		if [ -e "./BuildServer/BuildServer.jar" ];then
-			echo -e "\033[32mFile has been found / 文件已找到\n./BuildServer/BuildServer.jar\033[0m"
-			$0 $2
-			exit
-		else
-			echo -e "Cannot find \"BuildServer.jar\" in \"BuildServer\" directory.\n无法在“BuildServer”目录下找到“BuildServer.jar”文件。"
+	for setpath in "./BuildServer/BuildServer.jar" "./buildserver/buildserver.jar" "./BuildServer/buildserver.jar" "./buildserver/BuildServer.jar" "$StDiR/buildserver.jar" "./BuildServer.jar" none
+	do
+		if [ $setpath = "none" ];then
+			echo -e "Cannot find \"BuildServer.jar\",please input the full path. (support ./ & ../)\n无法找到“BuildServer.jar”文件，请手动输入完整路径。（支持 ./ 和 ../）"
+			$0 chkdir2 $2
+		elif [ ! "$($0 chkdir1 $setpath $2 back)" = "none" ];then
+			$0 chkdir1 $setpath $2
+			break
+		fi
+	done
+	exit
+elif [ "$1" = "chkdir1" ];then
+	if [ "$4" = "back" ];then
+		if [ ! -e "$2" ];then
+			echo "none"
 		fi
 	else
-		echo -e "Cannot find \"BuildServer\" folder in current directory, please check whether the current directory where AI2 is located\n无法在当前目录下找到“BuildServer”文件夹，请检查当前目录是否为 AI2 所在目录。"
+		if [ -d "$2" ];then
+			echo "This is a directory. / 这是一个文件夹。"
+			$0 chkdir2 $2
+		elif [ -e "$2" ];then
+		filepath=$(realpath $2)
+			echo -e "\033[32mFile has been found / 文件已找到\n$filepath\033[0m"
+			$0 $3 $filepath
+			exit
+		else
+			echo "Cannot find this file. / 找不到文件。"
+			$0 chkdir2 $2
+		fi
+	fi
+	exit
+elif [ "$1" = "chkdir2" ];then
+	read -p "Path / 路径 :" setpath
+	if [ $setpath ];then
+		$0 chkdir1 $setpath $2
+	else
+		$0 $1 $2
 	fi
 	exit
 elif [ "$1" = "Step1" ];then
-	echo -e "\033[34m\nChecking required program......\n检查所需的程序......\033[0m"
+	echo -e "\033[34m\nChecking required program / 检查所需的程序......\033[0m"
 	if [ "$($0 search aapt)" = "none" ] ;then
 		$0 checkn aapt
 		exit
@@ -57,7 +83,7 @@ elif [ "$1" = "Step1" ];then
 		$0 choice Step2
 	else
 		$0 checky aapt2
-		$0 Step2
+		$0 Step2 $2
 	fi
 	exit
 elif [ "$1" = "Step2" ];then
@@ -69,63 +95,66 @@ elif [ "$1" = "Step2" ];then
 	fi
 
 	echo -e "\n\033[1m\033[32mRequired program has been installed, It will into patching step now.\n所需的程序已经安装，现在将进入修补阶段。\033[0m"
-	$0 choice RepackStart
+	$0 choice RepackStart $2
 	exit
 elif [ "$1" = "RepackStart" ];then
-	StDiR=$(pwd)
-	TmPdIr=TempOfBugteas
+	TmPdIr=$(pwd)/TempOfBugteas
 	BuD=BuildServer
-	echo -e "\033[34mCreate temp directory......\n创建临时目录......\033[0m"
+	echo -e "\033[34mCreate temp directory / 创建临时目录 ......\033[0m"
 	mkdir -v $TmPdIr
 	cd $TmPdIr
-	echo -e "\033[34m\nUnpacking \"BuildServer.jar\" file......\n正在解包 “BuildServer.jar” 文件......\033[0m"
-	UnP="jar -xvf $StDiR/$BuD/BuildServer.jar"
+	echo -e "\033[34m\nUnpacking file / 正在解包文件 ......\033[0m"
+	UnP="jar -xvf $2"
 	echo "$UnP"
 	UnPsT=$($UnP)
-	if [ -d "$StDiR/$TmPdIr/tools/" ];then
+	if [ -d "$TmPdIr/tools/" ];then
 		echo -e "\033[32mUnpacking successful. / 解包成功。\033[0m"
 	else
 		echo -e "$UnPsT\n\n\033[41;39mUnpacking failed. / 解包失败。\033[0m"
+		cd ../
+		rm -rf $TmPdIr
 		exit
 	fi
 	echo 
-	if [ -e "$StDiR/$BuD/BuildServer.bak" ];then
-		echo -e "\033[34mRemove older \"BuildServer.jar\" file......\n正在删除旧的 “BuildServer.jar” 文件......\033[0m"
-		rm -v $StDiR/$BuD/BuildServer.jar
+	if [ -e "$2.bak" ];then
+		echo -e "\033[34mRemove older file / 正在删除旧的文件 ......\033[0m"
+		rm -v $2
 	else
-		echo -e "\033[34mBackup original \"BuildServer.jar\" file......\n正在备份原有 “BuildServer.jar” 文件......\033[0m"
-		mv -v $StDiR/$BuD/BuildServer.jar $StDiR/$BuD/BuildServer.bak
+		echo -e "\033[34mBackup original file / 正在备份原有文件 ......\033[0m"
+		mv -v $2 $2.bak
 	fi
-	echo -e "\033[34m\nReplacing files......\n正在替换文件......\033[0m"
-	if [ -e "$StDiR/$TmPdIr/tools/linux/aapt" ];then
-		rm -v $StDiR/$TmPdIr/tools/linux/aapt
-		cp -v $(../$0 search aapt) $StDiR/$TmPdIr/tools/linux/
+	echo -e "\033[34m\nReplacing files / 正在替换文件 ......\033[0m"
+	if [ -e "$TmPdIr/tools/linux/aapt" ];then
+		rm -v $TmPdIr/tools/linux/aapt
+		cp -v $(../$0 search aapt) $TmPdIr/tools/linux/
 	fi
-	if [ -e "$StDiR/$TmPdIr/tools/linux/aapt2" ];then
+	if [ -e "$TmPdIr/tools/linux/aapt2" ];then
 
 		if [ ! "$(../$0 search aapt2)" = "none" ] ;then
-			rm -v $StDiR/$TmPdIr/tools/linux/aapt2
-			cp -v $(../$0 search aapt2) $StDiR/$TmPdIr/tools/linux/
+			rm -v $TmPdIr/tools/linux/aapt2
+			cp -v $(../$0 search aapt2) $TmPdIr/tools/linux/
 		fi
 	fi
 	if [ -e "$StDiR/$TmPdIr/tools/linux/zipalign" ];then
-		rm -v $StDiR/$TmPdIr/tools/linux/zipalign
-		cp -v $(../$0 search zipalign) $StDiR/$TmPdIr/tools/linux/
+		rm -v $TmPdIr/tools/linux/zipalign
+		cp -v $(../$0 search zipalign) $TmPdIr/tools/linux/
 	fi
-	echo -e "\033[34m\nrepacking \"BuildServer.jar\" file......\n正在重新打包 “BuildServer.jar” 文件......\033[0m"
-	RpK="jar -cvf $StDiR/$BuD/BuildServer.jar *"
+	echo -e "\033[34m\nRepacking file / 正在重新打包文件 ......\033[0m"
+	RpK="jar -cvf $2 *"
 	echo "$RpK"
 	RpKsT=$($RpK)
-	if [ -e "$StDiR/$BuD/BuildServer.jar" ];then
+	if [ -e "$2" ];then
 		echo -e "\033[32mPacking successful. / 打包成功。\033[0m"
 	else
-		echo -e "$UpKsT\n\n\033[41;39mPacking failed. / 打包失败。\033[0m"
+		echo -e "$RpKsT\n\n\033[41;39mPacking failed. / 打包失败。\033[0m"
+		cd ../
+		rm -rf $TmPdIr
 		exit
 	fi
-	echo -e "\n\033[34mRemoving temp directory......\n删除临时目录......\033[0m"
+	echo -e "\n\033[34mRemoving temp directory / 删除临时目录 ......\033[0m"
 	rm -rf *
 	cd ../
-	rmdir -v $StDiR/$TmPdIr
+	rmdir -v $TmPdIr
 	echo -e "\033[1m\033[32m\nPatching completed.\nPlease do not uninstall aapt or zipalign to avoid installation package export failed.\nIf you have update the aapt or zipalign, please run this script again.\n修补完成。\n请不要卸载 aapt 或 zipalign 以免安装包导出失败。\n如果您更新了 aapt 或 zipalign，请再次运行这个脚本。\033[0m"
 	exit
 else
